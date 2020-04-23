abb47fe7f07ae8f095810a13e3147eec515e214e
brokenhoax <brokenhoax@outlook.com>
2020-04-23 02:00:57 -0500

    Added Event Listener

--- script.js
+++ script.js
@@ -1,113 +1,104 @@
 // // Assignment Code
-// // var generateBtn = document.querySelector("#generate");
 
-const passwordEl = document.getElementById('password');
-const generateBtn = document.getElementById('generate');
+// const randomFunc = {
+// 	lower: getRandomLower,
+// 	upper: getRandomUpper,
+// 	number: getRandomNumber,
+// 	symbol: getRandomSymbol
+// }
+
+var passwordEl = document.getElementById('password');
+var generateBtn = document.getElementById('generate');
+
+
+// Prompt User for Input
+
+function getLength() {
+  do { 
+    var pwLengthReq = parseInt(window.prompt("Please enter a number from 8 to 128"));
+  } 
+  while(isNaN(pwLengthReq) || pwLengthReq > 128 || pwLengthReq < 8);
+
+  return pwLengthReq;
+}
+// var length = getLength();
+// console.log(length);
 
 
 // Generate Random Values Functions
 
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  var pwLowerReq = confirm("Click 'Ok' to include Lower-Case Characters. Click 'Cancel' to disclude Lower-Case Characters.");
+  if (pwLowerReq === true) {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+    else {
+      return '';
+    }
 }
+// var lower = getRandomLower();
+// console.log(lower);
 
 function getRandomUpper() {
+  var pwUpperReq = confirm("Click 'Ok' to include Upper-Case Characters. Click 'Cancel' to disclude Upper-Case Characters.");
+  if (pwUpperReq === true) {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+    else {
+      return '';
+    }
 }
+// var upper = getRandomUpper();
+// console.log(upper);
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  var pwNumReq = confirm("Click 'Ok' to include Numbers. Click 'Cancel' to disclude Numbers.");
+  if (pwNumReq === true) {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+    else {
+      return '';
+    }
 }
+// var number = getRandomNumber();
+// console.log(number);
 
 function getRandomSymbol() {
+  var pwSymReq = confirm("Click 'Ok' to include Special Characters. Click 'Cancel' to disclude Special Characters.");
   const symbols = '!@#$%^&*()=+[{]},<.>/?'
-  return symbols[Math.floor(Math.random() * symbols.length)];
+  if (pwSymReq === true) {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+    }
+    else {
+      return '';
+    }
 }
-
-console.log(getRandomLower());
-console.log(getRandomUpper());
-console.log(getRandomNumber());
-console.log(getRandomSymbol());
+// var symbol = getRandomSymbol();
+// console.log(symbol);
 
 
-// Prompt User for Input
+// Generate Password Function
 
-function generateLength() {
-  do { 
-    var pwLengthReq = parseInt(window.prompt("Please enter a number from 8 to 128"));
-  } 
-  while(isNaN(pwLengthReq) || pwLengthReq > 128 || pwLengthReq < 8);
-  
-
-  console.log(pwLengthReq);
-
-  return this.pwLengthReq;
-});
-
-
-generateBtn.addEventListener('click', function generateCriteria() {
-
-  const pwSymReq = confirm("Click 'Ok' to include Special Characters. Click 'Cancel' to disclude Special Characters.");
-  const pwUpperReq = confirm("Click 'Ok' to include Upper-Case Characters. Click 'Cancel' to disclude Upper-Case Characters.");
-  const pwLowerReq = confirm("Click 'Ok' to include Lower-Case Characters. Click 'Cancel' to disclude Lower-Case Characters.");
-  const pwNumReq = confirm("Click 'Ok' to include Numbers. Click 'Cancel' to disclude Numbers.");
-
-  console.log(pwSymReq);
-  console.log(pwUpperReq);
-  console.log(pwLowerReq);
-  console.log(pwNumReq);
-});
-
-// const length = pwLengthReq.value;
-// const hasLower = pwLowerReq.true;
-// const hasUpper = pwUpperReq.true;
-// const hasNumber = pwNumReq.true;
-// const hasSymbol = pwSymReq.true;
-// passwordEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+function generatePassword(){
+  var length = getLength();
+  var symbol = getRandomSymbol();
+  var lower = getRandomLower();
+  var upper = getRandomUpper();
+  var number = getRandomNumber();
+  var generatedPassword = [];
+  for (i = 0; i <= length; i++) {
+    generatedPassword = generatedPassword.push(lower, upper, number, symbol);
+    return generatedPassword; 
+  }
+}
+// var finalPassword = generatePassword();
+//   console.log(finalPassword);
 
 
-  lower = getRandomLower();
-  upper = getRandomUpper();
-  number = getRandomNumber();
-  symbol = getRandomSymbol();
-  length = generateLength();
+// Write Password To Screen
 
-
-// // // Generate Password Function
-
-// function generatePassword(lower, upper, number, symbol, length) {
-//   let generatedPassword = '';
-//   const typesCount = getRandomLower + getRandomUpper + getRandomNumber + getRandomSymbol; 
-//   console.log('typesCount: ', typesCount);
-//   const typesArr = [{ getRandomLower }, {getRandomUpper}, {getRandomNumber}, {getRandomSymbol}].filter(item => Object.values(item)[0]);
-//   console.log('typesaArr: ', typesArr);
-
-
-//   // Doesn't have a selected type 
-
-//   if(typesCount === 0) {
-//     return '';
-//   } 
-
-//   // Creates a Loop
-
-//   for(let i=0; i<length; i+=typesCount) {
-//     typesArr.forEach(type => {
-//       const funcName = Object.keys(type)[0];
-//       generatedPassword += pwFunc[funcName]();
-//     });
-//   }
-
-//   const finalPassword = generatedPassword.slice(0, length);
-
-//   return finalPassword;
-// }
-
-
-// // Write password to the #password input
-
-// generateBtn.addEventListener("click", function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-//   passwordText.value = password;
-// });
+generateBtn.addEventListener("click", function writePassword() {
+  var finalPassword = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = finalPassword;
+}); 
